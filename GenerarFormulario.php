3cb9b59fa8e79641c4b39482244f3958c3dd0b93
roberto nicolas herrera jorquera <robertherrerajorquera@gmail.com>
2019-11-19 20:30:46 -0300

    Cambio de nombre
    Se cambio el nombre a GenerarFormualrio.php

--- GenerarFormulario.php
+++ GenerarFormulario.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="js/bootstrap.js"></script>
     <link rel="stylesheet" href="css/bootstrap.css">
-    <title>Document</title>
+    <title>Generar Formulario</title>
 </head>
 <body>
     <div class="topnav" id="myTopnav">
